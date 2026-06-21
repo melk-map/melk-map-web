5b9bc7bb43db5f8ff034054968f608203271a4c4
pmoieni <62774242+pmoieni@users.noreply.github.com>
2026-06-21 11:31:24 +0330

    feat: latest changes

--- src/lib/components/pages/home/Map.svelte
+++ src/lib/components/pages/home/Map.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import maplibregl, { type Map } from 'maplibre-gl';
-	import 'maplibre-gl/dist/maplibre-gl.css';
-	import { Protocol } from 'pmtiles';
-	import testLocs from './testLocs';
-	import axios from 'axios';
+	import { onMount } from "svelte";
+	import maplibregl, { type Map } from "maplibre-gl";
+	import "maplibre-gl/dist/maplibre-gl.css";
+	import { Protocol } from "pmtiles";
+	import testLocs from "./testLocs";
+	import axios from "axios";
+	import { latLngToCell } from "h3-js";
 
 	interface Props {
 		coordinates: [number, number];
@@ -31,7 +32,7 @@
 		return Math.floor(
 			((zoom - minZoom) / (maxZoom - minZoom)) *
 				((maxZoom - minZoom) / (maxResolution - minResolution)) +
-			minResolution
+				minResolution,
 		);
 	}
 
@@ -39,65 +40,45 @@
 		if (!mapContainer) return;
 
 		let protocol = new Protocol();
-		maplibregl.addProtocol('pmtiles', protocol.tile);
+		maplibregl.addProtocol("pmtiles", protocol.tile);
 
 		maplibregl.setRTLTextPlugin(
-			'https://unpkg.com/@mapbox/mapbox-gl-rtl-text@0.3.0/dist/mapbox-gl-rtl-text.js',
-			true // Lazy load the plugin
+			"https://unpkg.com/@mapbox/mapbox-gl-rtl-text@0.3.0/dist/mapbox-gl-rtl-text.js",
+			true, // Lazy load the plugin
 		);
 
 		map = new maplibregl.Map({
 			container: mapContainer,
-			style: '/map/styles/light.json',
+			style: "/map/styles/light.json",
 			center: coordinates,
 			attributionControl: false,
 			minZoom,
 			maxZoom,
-			zoom: 10
+			zoom: 10,
 		});
 
 		// add openstreetmap attributions back
-		map.addControl(new maplibregl.AttributionControl({ compact: true }), 'bottom-left');
+		map.addControl(
+			new maplibregl.AttributionControl({ compact: true }),
+			"bottom-left",
+		);
 
 		// update map boundaries on moveend
-		map.on('moveend', async () => {
-			const bounds = map!.getBounds();
+		map.on("moveend", async () => {
+			const center = map!.getCenter();
 			const zoom = map!.getZoom();
 			const h3Resolution = zoomToResolution(zoom);
 
-			const reqData = {
-				sw: {
-					long: bounds.getSouthWest().lng,
-					lat: bounds.getSouthWest().lat
-				},
-				nw: {
-					long: bounds.getNorthEast().lng,
-					lat: bounds.getNorthEast().lat
-				},
-				resolution: h3Resolution
-			};
-
-			try {
-				const response = await axios.post(
-					'http://localhost:1234/properties/h3',
-					JSON.stringify(reqData) /*{
-					method: 'POST',
-					headers: { 'Content-Type': 'application/json' },
-					body: JSON.stringify(reqData)
-				}*/
-				);
-
-				console.log(`Received ${response.data.count} hexagons at resolution ${h3Resolution}`);
-			} catch (error) {
-				console.error('Failed to fetch hexagons:', error);
-			}
+			console.log(latLngToCell(center.lat, center.lng, 9));
 		});
 
 		for (const location of testLocs) {
-			new maplibregl.Marker().setLngLat([location[0], location[1]]).addTo(map);
+			new maplibregl.Marker()
+				.setLngLat([location[0], location[1]])
+				.addTo(map);
 		}
 
-		map.on('load', () => {
+		map.on("load", () => {
 			if (map) {
 				map.resize();
 			}
